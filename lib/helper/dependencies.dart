887ea52a242bf0594e692ecdb0adb0fc7eb68277
thanhthanDUT <thanhthan2k1@gmail.com>
2022-12-07 14:02:40 +0700

    test app

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,21 +1,21 @@
+import 'package:cdcn/controllers/cart_controller.dart';
+import 'package:cdcn/controllers/foodDetail_controller.dart';
+import 'package:cdcn/controllers/foodOfStore_controller.dart';
+import 'package:cdcn/controllers/myOrdered_controller.dart';
+import 'package:cdcn/controllers/payment_controller.dart';
+import 'package:cdcn/controllers/recommended_storenear_controller.dart';
+import 'package:cdcn/controllers/user_controller.dart';
+import 'package:cdcn/data/api/api_client.dart';
+import 'package:cdcn/data/repository/cart_repo.dart';
+import 'package:cdcn/data/repository/foodDetail_repo.dart';
+import 'package:cdcn/data/repository/foodOfStore_repo.dart';
+import 'package:cdcn/data/repository/myOrdered_repo.dart';
+import 'package:cdcn/data/repository/payment_repo.dart';
+import 'package:cdcn/data/repository/recommended_storenear_repo.dart';
+import 'package:cdcn/data/repository/user_repo.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../controllers/cart_controller.dart';
-import '../controllers/foodDetail_controller.dart';
-import '../controllers/foodOfStore_controller.dart';
-import '../controllers/myOrdered_controller.dart';
-import '../controllers/payment_controller.dart';
-import '../controllers/recommended_storenear_controller.dart';
-import '../controllers/user_controller.dart';
-import '../data/api/api_client.dart';
-import '../data/repository/cart_repo.dart';
-import '../data/repository/foodDetail_repo.dart';
-import '../data/repository/foodOfStore_repo.dart';
-import '../data/repository/myOrdered_repo.dart';
-import '../data/repository/payment_repo.dart';
-import '../data/repository/recommended_storenear_repo.dart';
-import '../data/repository/user_repo.dart';
 Future<void> init() async {
   final sharePreferences=await SharedPreferences.getInstance();
   //await sharePreferences.remove("token");
@@ -34,7 +34,10 @@ Future<void> init() async {
   Get.lazyPut(() => FoodOfStoreController(foodOfStoreRepo: Get.find()));
   Get.lazyPut(() => RecommendedStoreNearRepo(apiClient: Get.find()));
   //controllers
-  Get.lazyPut(() => RecommendedStoreNearController(recommendedStoreNearRepo: Get.find()));
+  Get.lazyPut(() {
+    return RecommendedStoreNearController(recommendedStoreNearRepo: Get.find());
+  },
+      fenix: true);
   Get.lazyPut(() => FoodDetailRepo(apiClient: Get.find()));
   //controllers
   Get.lazyPut(() {
